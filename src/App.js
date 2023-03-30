9a0e8f9db97fa51d0fbab213be2292af61ed805a
Nils Grob <nils.grob@protonmail.com>
2023-03-30 10:58:00 +0200

    refactored webRTC stuff & #37 added STUN server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import Header from "components/views/Header";
 import AppRouter from "components/routing/routers/AppRouter";
+import { createContext, useEffect, useRef } from "react";
+import { WebSocketConnection } from "./helpers/webRTC";
+import { getWsUrl } from "helpers/getDomain";
 require("./helpers/webRTC");
 /**
  * Happy coding!
  * React Template by Lucas Pelloni
  * Overhauled by Kyrill Hux
  */
+export const WebSocketContext = createContext(null);
+
 const App = () => {
+
   return (
-    <div>
-      <Header height="100"/>
-      <AppRouter/>
-    </div>
+    <WebSocketContext.Provider value={new WebSocketConnection(getWsUrl())}>
+      <div>
+        <Header height="100" />
+        <AppRouter />
+      </div>
+    </WebSocketContext.Provider>
   );
 };
 
