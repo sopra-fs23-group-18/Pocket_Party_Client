b07ee4181302d7d253b94f1db7898e0dbe00415b
Sven Ringger <sven.ringger@hotmail.com>
2023-04-03 16:17:55 +0200

    Add PlayerDisplay

---
+++ src/components/views/PlayerDisplay.js
@@ -0,0 +1,10 @@
+import 'styles/views/PlayerDisplay.scss';
+
+const PlayerDisplay = () => {
+    return (
+        <div className="PlayerDisplay Container">
+            <label className="PlayerDisplay Name"></label>
+        </div>
+    );
+}
+export default PlayerDisplay;
