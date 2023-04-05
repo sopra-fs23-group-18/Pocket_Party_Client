dcac95d809a8e396dc8612d0cac1d2dd057698e7
Sven Ringger <sven.ringger@hotmail.com>
2023-04-05 16:47:31 +0200

    Add API call for minigame preview #19

--- src/components/views/GamePreview.js
+++ src/components/views/GamePreview.js
@@ -1,6 +1,21 @@
+import { api } from 'helpers/api';
 import 'styles/views/GamePreview.scss';
-//TODO: Add image of game
-const GamePreview = () => {
+
+const GamePreview = ({ id }) => {
+    const [description, setDescription] = useState('');
+    const [pointsToGain, setPointsToGain] = useState('');
+    async function getData() {
+        const response = await api.get(`'/lobbies/${id}/minigame'`);
+        const description = response.description;
+        const pointsToGain = response.scoreToGain;
+        pointsToGain += " pt";
+        setPointsToGain(pointsToGain || 'Points could not be loaded!')
+        setDescription(description || 'The description could not be loaded!');
+    }
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div className="Preview Container">
             <div className="Preview MinigamelabelBox">
@@ -8,15 +23,16 @@ const GamePreview = () => {
             </div>
             <div className="Preview MinigametitleBox">
                 <label className="Preview Minigametitle">Pong</label>
-                <label className="Preview MinigamePoints">500pt</label>
+                <label className="Preview MinigamePoints">{pointsToGain}</label>
             </div>
             <div>
                 <label className="Preview HTP">How to play</label>
             </div>
             <div className='Preview DescBox'>
-                <label className='Preview Description'>Description goes here</label>
+                <label className='Preview Description'>{description}</label>
             </div>
         </div>
     );
 }
+
 export default GamePreview;
