470a180c2efe13306a2c523bc8df60c43101defa
Sven Ringger <sven.ringger@hotmail.com>
2023-04-19 16:07:38 +0200

    Add GameWon screen #13

--- src/components/views/GameWon.js
+++ src/components/views/GameWon.js
@@ -1,7 +1,18 @@
-const GameWon = () => {
+import BaseContainer from "components/ui/BaseContainer";
+import GameHeader from "components/ui/GameHeader";
+import PlayerContainer from "components/ui/PlayerContainer";
+import "styles/views/GameWon.scss"
+const GameWon = ({ lobbyId }) => {
 
     return (
-        <label>hello</label>
+        <BaseContainer>
+            <GameHeader />
+            <div className="GameWon maindiv">
+                <label className="GameWon TWI">The winner is</label>
+                <PlayerContainer />
+                <PlayerContainer />
+            </div>
+        </BaseContainer>
     )
 }
 export default GameWon;
