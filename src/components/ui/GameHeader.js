ea77b34573fb44e5cba4b503148fccce0dcdd98f
Sven Ringger <sven.ringger@hotmail.com>
2023-04-19 15:07:50 +0200

    Add GameHeader component for unification #21

--- src/components/ui/GameHeader.js
+++ src/components/ui/GameHeader.js
@@ -1,11 +1,25 @@
 import 'styles/ui/GameHeader.scss'
 import BaseContainer from './BaseContainer';
-const GameHeader = () => {
+import { useState, useEffect } from 'react';
+import { api } from 'helpers/api';
+const GameHeader = ({ lobbyId }) => {
+    const [info, setInfo] = useState({ type: 'Not Found', scoreToGain: 'Not Found' });
+    useEffect(() => {
+        const get_data = async () => {
+            const data = await api.get(`lobbies/${lobbyId}/minigame`);
+            setInfo(data);
+        };
+
+        get_data();
+    }, [lobbyId]);
     return (
         <BaseContainer>
-            <label className="GameHeader Minigamelabel">Minigame</label>
+            <div className="GameHeader Minigamecontainer">
+                <label className='GameHeader Minigamelabel'>Minigame</label>
+            </div>
             <div className='GameHeader Minigametitlebox'>
-                <label className='GameHeader title'>Pong</label>
+                <label className='GameHeader title'>{info.type}</label>
+                <label className='GameHeader pts'>{info.scoreToGain}pt</label>
             </div>
         </BaseContainer>
     )
