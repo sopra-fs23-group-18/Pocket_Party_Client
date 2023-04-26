e6d0222eb22b6c84f69a0c7a0ab6328aea7aca21
Flip9750 <hassan.naseem@outlook.com>
2023-04-26 21:48:36 +0200

    #14 Implemented score overview screen

--- src/components/views/TeamScoreOverview.js
+++ src/components/views/TeamScoreOverview.js
@@ -1,56 +1,92 @@
-import 'styles/views/TeamScoreOverview.scss'
-import BaseContainer from 'components/ui/BaseContainer'
-import { useState, useEffect } from 'react';
+import "styles/views/TeamScoreOverview.scss";
+import BaseContainer from 'components/ui/BaseContainer';
+import { useState, useEffect, useRef, useContext } from 'react';
 import { api } from 'helpers/api';
 import HeaderContainer from 'components/ui/HeaderContainer';
 import { useHistory } from 'react-router-dom';
+import { LobbyContext } from "components/routing/routers/AppRouter";
+
 const TeamScoreOverview = () => {
-    /*const [info, setInfo] = useState({ type: 'Not Found', scoreToGain: 'Not Found' });
-    useEffect(() => {
-        const get_data = async () => {
-            const data = await api.get(`lobbies/${lobbyId}/minigame`);
-            setInfo(data);
-        };
-
-        get_data();
-    }, [lobbyId]);*/
-    const [Team1Pts, setTeam1Pts] = useState("80%");
-    const [Team2Pts, setTeam2Pts] = useState("20%");
-    const updateScoreTeam1 = () => {
-        //TODO: API call
-        const result = 80;
-        setTeam1Pts(result)
-
-    };
-    const updateScoreTeam2 = () => {
-        //TODO: API call
-        const result = 80;
-        setTeam2Pts(result)
-
-    }
     const navigation = useHistory();
+    const lobbyContext = useContext(LobbyContext);
+    // State for team scores
+    // const [team1PtsOvr, setTeam1PtsOvr] = useState(0);
+    // const [team2PtsOvr, setTeam2PtsOvr] = useState(0);
+    const [team1Pts, setTeam1Pts] = useState(0);
+    const [team2Pts, setTeam2Pts] = useState(0);
+    const [data, setData] = useState(null);
+
+    // Refs for triggering animations
+    const team1BarRef = useRef(null);
+    const team2BarRef = useRef(null);
+
+    const getPoints = async () => {
+        const response = await api.get(`/lobbies/${lobbyContext.lobby.id}/scores`);
+        setData(response.data);
+        console.log(response.data)
+    }
+    // Update team scores and trigger animations
+    const updateScoreTeam1 = async () => {
+        const score = data.teams[0].score;
+        console.log(score);
+        setTeam1Pts(score);
+        team1BarRef.current.classList.add('mounted');
+    };
+
+    const updateScoreTeam2 = async () => {
+        const score = data.teams[1].score;
+        console.log(score);
+        setTeam2Pts(score);
+        team2BarRef.current.classList.add('mounted');
+    };
+
     useEffect(() => {
-        setTimeout(() => {
-            navigation.push("/gamePreview")
+        getPoints();
+    }, []);
+    // Wait for data to be updated
+    useEffect(() => {
+        if (data) {
+            updateScoreTeam1();
+            updateScoreTeam2();
+        }
+    }, [data]);
+
+    // Automatically redirect after 10 seconds
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            navigation.push('/gamePreview');
         }, 10000);
-    }, [])
+        return () => clearTimeout(timeoutId);
+    }, [navigation]);
+
+
     return (
         <BaseContainer>
-            <HeaderContainer title='Minigame Score' text='Progress'></HeaderContainer>
-            <div className='tso maincontent'>
-                <div className='tso barbox'>
-                    <label className='tso team1'>Team 1</label>
-                    <div className='tso team1Progress' style={{ backgroundColor: '#FF5733', width: `${Team1Pts}`, height: "5em" }}></div>
-                    <label className='tso team1Pts'>{Team1Pts}</label>
+            <HeaderContainer title="Minigame Score" text="Progress" />
+            <div className="tso maincontent">
+                <div className="tso barbox">
+                    <label className="tso team1">Team 1</label>
+                    <div
+                        ref={team1BarRef}
+                        className="tso team1-bar"
+                        style={{ width: `${team1Pts}%` }}
+                    >
+                    </div>
+                    <label className="tso team1">{team1Pts}%</label>
 
-                    <label className='tso team2'>Team 2</label>
-
-                    <div className='tso team2Progress' style={{ backgroundColor: "#00BFFF", width: `${Team2Pts}`, height: "5em" }}></div>
-                    <label className='tso team2Pts'>{Team2Pts}</label>
+                    <label className="tso team2">Team 2</label>
+                    <div
+                        ref={team2BarRef}
+                        className="tso team2-bar"
+                        style={{ width: `${team2Pts}%` }}
+                    >
+                    </div>
+                    <label className="tso team2">{team2Pts}%</label>
                 </div>
-                <div className='tso line'></div>
+                <div className="tso line" />
             </div>
         </BaseContainer>
-    )
-}
+    );
+};
+
 export default TeamScoreOverview;
