f0f6d32699b712a586ded6f3db6271fb260e3ca7
Byron Wu <wuguoaiw@gmail.com>
2023-05-09 19:59:05 +0200

    Command of Pong to mobile #66

--- src/models/Input.js
+++ src/models/Input.js
@@ -2,6 +2,8 @@ class Input {
     constructor(data = {}) {
       this.inputType = null;
       this.rawData = null;
+      this.count = null;
+      this.degree = null;
       Object.assign(this, data);
     }
   }
