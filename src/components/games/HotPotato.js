77690413f7d6fcf06e226fa9b25e31c695a58860
Flip9750 <hassan.naseem@outlook.com>
2023-05-10 21:38:43 +0200

    commented hotPotato

--- src/components/games/HotPotato.js
+++ src/components/games/HotPotato.js
@@ -1,139 +1,139 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const HotPotato = () => {
-    const [timeLeft, setTimeLeft] = useState(10);
-    const [currentPlayer, setCurrentPlayer] = useState(0);
-    const [players, setPlayers] = useState(['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5']);
-    const [gameOver, setGameOver] = useState(false);
-    const [index, setIndex] = useState(0);
-    const [hasCooldown, setHasCooldown] = useState(true);
-
-
-    useEffect(() => {
-        if (connections.stompConnection.state === ActivationState.ACTIVE) {
-            if (connections.stompConnection.state === ActivationState.ACTIVE) {
-                players.forEach(player => {
-                    connections.stompConnection.publish({
-                        destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
-                        body: JSON.stringify({
-                            signal: "START",
-                            minigame: "TAPPING_GAME"
-                        })
-                    });
-                });
-            }
-
-        }
-        return () => {
-            if (connections.stompConnection.state === ActivationState.ACTIVE) {
-                players.forEach(player => {
-                    connections.stompConnection.publish({
-                        destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
-                        body: JSON.stringify({
-                            signal: "STOP",
-                            minigame: "HOTPOTATO"
-                        })
-                    });
-                });
-            }
-
-        }
-    }, [])
-    useEffect(() => {
-
-        if (connections.stompConnection.state === ActivationState.ACTIVE) {
-            players.forEach(player => {
-                connections.stompConnection.subscribe(`/topic/lobbies/${lobbyContext.lobby.id}/players/${player.id}/input`, handlePass);
-            });
-            return;
-        }
-
-        connections.stompConnection.onConnect = (_) => {
-            players.forEach(player => {
-                connections.stompConnection.subscribe(`/topic/lobbies/${lobbyContext.lobby.id}/players/${player.id}/input`, handlePass);
-            });
-
-            players.forEach(player => {
-                connections.stompConnection.publish({
-                    destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
-                    body: JSON.stringify({
-                        signal: "START",
-                        minigame: "HOTPOTATO"
-                    })
-                });
-            });
-        };
-    }, [connections, lobbyContext, minigameContext]);
+// const HotPotato = () => {
+//     const [timeLeft, setTimeLeft] = useState(10);
+//     const [currentPlayer, setCurrentPlayer] = useState(0);
+//     const [players, setPlayers] = useState(['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5']);
+//     const [gameOver, setGameOver] = useState(false);
+//     const [index, setIndex] = useState(0);
+//     const [hasCooldown, setHasCooldown] = useState(true);
 
 
+//     useEffect(() => {
+//         if (connections.stompConnection.state === ActivationState.ACTIVE) {
+//             if (connections.stompConnection.state === ActivationState.ACTIVE) {
+//                 players.forEach(player => {
+//                     connections.stompConnection.publish({
+//                         destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
+//                         body: JSON.stringify({
+//                             signal: "START",
+//                             minigame: "TAPPING_GAME"
+//                         })
+//                     });
+//                 });
+//             }
+
+//         }
+//         return () => {
+//             if (connections.stompConnection.state === ActivationState.ACTIVE) {
+//                 players.forEach(player => {
+//                     connections.stompConnection.publish({
+//                         destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
+//                         body: JSON.stringify({
+//                             signal: "STOP",
+//                             minigame: "HOTPOTATO"
+//                         })
+//                     });
+//                 });
+//             }
+
+//         }
+//     }, [])
+//     useEffect(() => {
+
+//         if (connections.stompConnection.state === ActivationState.ACTIVE) {
+//             players.forEach(player => {
+//                 connections.stompConnection.subscribe(`/topic/lobbies/${lobbyContext.lobby.id}/players/${player.id}/input`, handlePass);
+//             });
+//             return;
+//         }
+
+//         connections.stompConnection.onConnect = (_) => {
+//             players.forEach(player => {
+//                 connections.stompConnection.subscribe(`/topic/lobbies/${lobbyContext.lobby.id}/players/${player.id}/input`, handlePass);
+//             });
+
+//             players.forEach(player => {
+//                 connections.stompConnection.publish({
+//                     destination: `/lobbies/${lobbyContext.lobby.id}/players/${player.id}/signal`,
+//                     body: JSON.stringify({
+//                         signal: "START",
+//                         minigame: "HOTPOTATO"
+//                     })
+//                 });
+//             });
+//         };
+//     }, [connections, lobbyContext, minigameContext]);
 
 
 
 
 
 
-    useEffect(() => {
-        if (timeLeft === 0) {
-            handleExplode();
-        }
-        const timer = setTimeout(() => {
-            setTimeLeft(timeLeft - 1);
-        }, 1000);
-        return () => clearTimeout(timer);
-    }, [timeLeft]);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setHasCooldown(false);
-        }, 1000)
-    }, [currentPlayer]);
 
-    const endTurn = () => {
-        if (hasCooldown === false) {
-            setNextPlayer(currentPlayer);
-            //TODO Sven: set mobile screen for player who has passed the bomb
-        }
-    };
+//     useEffect(() => {
+//         if (timeLeft === 0) {
+//             handleExplode();
+//         }
+//         const timer = setTimeout(() => {
+//             setTimeLeft(timeLeft - 1);
+//         }, 1000);
+//         return () => clearTimeout(timer);
+//     }, [timeLeft]);
 
-    const setNextPlayer = (currentIndex) => {
-        const newIndex = getNewIndex(currentIndex);
-        setIndex(newIndex);
-        setCurrentPlayer(newIndex);
-        setHasCooldown(true);
-        //TODO Sven: set mobile screen for player who received the bomb
-    };
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setHasCooldown(false);
+//         }, 1000)
+//     }, [currentPlayer]);
 
-    const getNewIndex = (index) => {
-        const newIndex = Math.floor(Math.random() * players.length);
-        if (newIndex != index) {
-            return newIndex
-        }
-        return getNewIndex(index)
-    };
+//     const endTurn = () => {
+//         if (hasCooldown === false) {
+//             setNextPlayer(currentPlayer);
+//             //TODO Sven: set mobile screen for player who has passed the bomb
+//         }
+//     };
 
-    const handlePass = () => {
-        endTurn();
-    };
+//     const setNextPlayer = (currentIndex) => {
+//         const newIndex = getNewIndex(currentIndex);
+//         setIndex(newIndex);
+//         setCurrentPlayer(newIndex);
+//         setHasCooldown(true);
+//         //TODO Sven: set mobile screen for player who received the bomb
+//     };
 
-    const handleExplode = () => {
-        setGameOver(true);
-    };
+//     const getNewIndex = (index) => {
+//         const newIndex = Math.floor(Math.random() * players.length);
+//         if (newIndex != index) {
+//             return newIndex
+//         }
+//         return getNewIndex(index)
+//     };
 
-    return (
-        <div>
-            <h1>Hot Potato</h1>
-            {!gameOver ? (
-                <div>
-                    <p>Time Left: {timeLeft}</p>
-                    <p>Current Player: {players[currentPlayer]}</p>
-                    <button onClick={handlePass}>Pass</button>
-                    <button onClick={handleExplode}>Explode</button>
-                </div>
-            ) : (
-                <><p>Game Over</p><label>{players[currentPlayer]} lost</label></>
-            )}
-        </div>
-    );
-};
+//     const handlePass = () => {
+//         endTurn();
+//     };
 
-export default HotPotato;
+//     const handleExplode = () => {
+//         setGameOver(true);
+//     };
+
+//     return (
+//         <div>
+//             <h1>Hot Potato</h1>
+//             {!gameOver ? (
+//                 <div>
+//                     <p>Time Left: {timeLeft}</p>
+//                     <p>Current Player: {players[currentPlayer]}</p>
+//                     <button onClick={handlePass}>Pass</button>
+//                     <button onClick={handleExplode}>Explode</button>
+//                 </div>
+//             ) : (
+//                 <><p>Game Over</p><label>{players[currentPlayer]} lost</label></>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default HotPotato;
